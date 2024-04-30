c578e91f3e110898ac0ad04067e4b322ce9da931
EmmetCaulfield <emmet-github@caulfield.info>
2024-04-30 14:40:17 +0100

    Added exchange rate utility.

--- Makefile
+++ Makefile
@@ -1,3 +1,16 @@
+include credentials.mk
+
+xebase := https://xecdapi.xe.com/v1/historic_rate/period/
+xefrom := USD
+xeto   := GBP,EUR,SGD
+period := -30 days
+today  := $(shell date --iso)
+fado   := $(shell date -d'$(period)' --iso)
+xeurl  := $(xebase)?from=$(xefrom)&to=$(xeto)&start_timestamp=$(fado)&end_timestamp=$(today)
+
+xrates.json:
+	curl -u '$(user):$(pass)' '$(xeurl)' | json_pp | tee $@
+
 .PHONY: default
 default:
 	@echo No default
@@ -5,3 +18,7 @@ default:
 .PHONY: clean
 clean:
 	rm -f *~
+
+.PHONY: pristine
+pristine: clean
+	rm -f xrates.json
---
+++ xrates.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+from statistics import mean, median
+
+                
+def die(msg: str, code: int=2):
+    print(f'FATAL: {msg}')
+    sys.exit(code)
+
+def usage():
+    print("USAGE: xrates <amount> <currency-code>")
+    sys.exit(1)
+
+class Currency():
+    def __init__(self:object, sym: str, amt: float) -> None:
+        self.from_sym = sym
+        self.amount = float(amt)
+        self.to_sym = {}
+
+    def to(self: object, sym: str, val: float) -> None:
+        if sym not in self.to_sym:
+            self.to_sym[sym] = []
+        self.to_sym[sym].append(float(val)/float(self.amount))
+
+    def mean(self: object, sym: str, dp:int = 4) -> float:
+        return round(mean(self.to_sym[sym]), dp)
+
+    def median(self: object, sym: str) -> float:
+        return round(median(self.to_sym[sym]), dp)
+
+    def max(self: object, sym: str, dp:int = 4) -> float:
+        return round(max(self.to_sym[sym]), dp)
+
+    def min(self: object, sym: str, dp:int = 4) -> float:
+        return round(min(self.to_sym[sym]), dp)
+
+    def invert(self: object, sym: str) -> object:
+        if sym == self.from_sym:
+            return self
+        if sym not in self.to_sym:
+            return None
+        inv = Currency(sym, 1.0)
+        inv.to_sym[self.from_sym] = [1.0/x for x in self.to_sym[sym]]
+        for s in self.to_sym.keys():
+            if s == sym:
+                continue
+            else:
+                inv.to_sym[s] = [y/x for (x,y) in zip(self.to_sym[sym], self.to_sym[s])]
+        return inv
+
+
+def loadCurrencyData(filename: str = 'xrates.json'):
+    with open(filename) as f:
+        try:
+            rates = json.load(f)
+        except Exception as e:
+            die(f"exception ({e}) loading JSON from file ({filename})")
+    amount = rates["amount"]
+    fromcur = rates["from"]
+    curr = Currency(fromcur, amount)
+    for sym in rates["to"].keys():
+        for datum in rates["to"][sym]:
+            curr.to(sym, float(datum["mid"]))
+    return curr
+            
+def main():
+    if len(sys.argv) != 3:
+        usage()
+    usd = loadCurrencyData()
+    amt = float(sys.argv[1])
+    cc  = sys.argv[2].upper()
+    cur = usd.invert(cc)
+    if cur is None:
+        die("Unknown currency code '{cc}'")
+    for code in cur.to_sym.keys():
+        lo = cur.min(code) * amt
+        mid = cur.mean(code) * amt
+        hi = cur.max(code) * amt
+        print(f"{code} {lo:.2f} {mid:.2f} {hi:.2f}")
+    
+if __name__ == "__main__":
+    main()
+ 
